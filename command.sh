2d8c844921be70e4de21ed8cf267532c5001b797
krayste <steve.kraynov@gmail.com>
2022-02-11 22:02:21 +1100

    Add list of commands used in steps 1-8

---
+++ command.sh
@@ -0,0 +1,22 @@
+# REPO_URL https://github.com/krayste/blackbird-task-1
+# Commands used in steps 1-8 
+
+# Create react app 
+npx create-react-app blackbird-task-1
+cd blackbird-task-1
+
+# Log into GitHub CLI 
+gh auth login
+
+# Create repo and checkout to the "update_logo" branch
+gh repo create
+git checkout -b update_logo
+
+# After making changes, commit and push them up
+git add .
+git commit -m "Update logo to Propeller and link to Dirtmate"
+git push --set-upstream origin update_logo
+
+# Create and then approve a PR
+gh pr create --title "Update logo and link"
+gh pr merge
