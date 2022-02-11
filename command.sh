4e6fa63f8d275e6d3690b4004461855b2670bade
krayste <steve.kraynov@gmail.com>
2022-02-11 22:08:54 +1100

    Add programmatic changing of logo and link

--- command.sh
+++ command.sh
@@ -13,6 +13,8 @@ gh repo create
 git checkout -b update_logo
 
 # After making changes, commit and push them up
+sed -i "" 's|img src={logo}|img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg"|g' src/App.js
+sed -i "" 's|href="https://reactjs.org"|href="https://www.propelleraero.com/dirtmate/"|g' src/App.
 git add .
 git commit -m "Update logo to Propeller and link to Dirtmate"
 git push --set-upstream origin update_logo
